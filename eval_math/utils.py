dfbd17dcffb6cba7baad54618206f1bc1607c55a
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2025-03-21 16:00:50 -0500

    Update utils.py

--- eval_math/utils.py
+++ eval_math/utils.py
@@ -174,6 +174,13 @@ PROMPT_TEMPLATES = {
         "{output}",
         "\n\n"
     )
+    "llama": (
+        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 26 Jul 2024\n\n<|eot_id|>"
+        "<|start_header_id|>user<|end_header_id|>\n\n{input} Let\'s think step by step and output the final answer within \\boxed{{}}.<|eot_id|>"
+        "<|start_header_id|>assistant<|end_header_id|>\n\n",
+        "{output}",
+        "\n\n",
+    ),
 }
 
 
