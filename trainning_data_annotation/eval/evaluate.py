95857224a2be2afefa367287bc9cd219da1c385a
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2025-03-27 14:42:55 -0500

    Update evaluate.py

--- trainning_data_annotation/eval/evaluate.py
+++ trainning_data_annotation/eval/evaluate.py
@@ -12,8 +12,145 @@ from eval.grader import *
 from utils.parser import *
 from utils.utils import load_jsonl
 from utils.python_executor import PythonExecutor
+def get_batch_scores(remain_codes, remain_gts):
+    samples = []
+    assert len(remain_codes) == len(remain_gts)
+    for i in range(len(remain_codes)):
+        samples.append(
+            {"gt": remain_gts[i],
+             "code": remain_codes[i]
+            }
+        )
+    if 'idx' in samples[0]:
+        samples = {sample['idx']: sample for sample in samples}.values()
+        samples = sorted(samples, key=lambda x: x['idx']) 
+    else:
+        samples = [dict(idx=idx, **sample) for idx, sample in enumerate(samples)]
+    prompt_type = 'cot'
+    # execute
+    if ('pred' not in samples[0]) or execute:
+        if "pal" in prompt_type:
+            executor = PythonExecutor(get_answer_expr="solution()")
+        else:
+            executor = PythonExecutor(get_answer_from_stdout=True)
 
+        for sample in tqdm(samples, desc="Execute"):
+            sample['pred'] = []
+            sample['report'] = []
+            for code in sample['code']:
+                pred, report = run_execute(executor, code, prompt_type, execute=True)
+                sample['pred'].append(pred)
+                sample['report'].append(report)
 
+    params = [(idx, pred, sample['gt']) for idx, sample in enumerate(samples) for pred in sample['pred']]
+
+    scores = []
+    timeout_cnt = 0 
+
+    with ProcessPool() as pool:
+        future = pool.map(math_equal_process, params, timeout=10)
+        iterator = future.result()
+        with tqdm(total=len(samples), desc="Evaluate") as progress_bar:
+            while True:
+                try:
+                    result = next(iterator)
+                    scores.append(result)
+                except StopIteration:
+                    break
+                except TimeoutError as error:
+                    print(error)
+                    scores.append(False)
+                    timeout_cnt += 1
+                except Exception as error:
+                    print(error.traceback)
+                    exit()
+                progress_bar.update(1) 
+
+    idx = 0
+    score_mat = []
+    for sample in samples:
+        sample['score'] = scores[idx: idx+len(sample['pred'])]
+        assert len(sample['score']) == len(sample['pred'])
+        score_mat.append(sample['score'])
+        idx += len(sample['pred'])
+
+    max_len = max([len(s) for s in score_mat])
+
+    for i, s in enumerate(score_mat):
+        if len(s) < max_len:
+            score_mat[i] = s + [s[-1]] * (max_len - len(s)) # pad
+
+    return [sam['score'] for sam in samples], [sam['pred'] for sam in samples]
+def get_scores(remain_codes, remain_gts):
+    samples = []
+    assert len(remain_codes) == len(remain_gts)
+    for i in range(len(remain_codes)):
+        samples.append(
+            {"gt": remain_gts[i],
+             "code": [remain_codes[i]]
+            }
+        )
+    if 'idx' in samples[0]:
+        samples = {sample['idx']: sample for sample in samples}.values()
+        samples = sorted(samples, key=lambda x: x['idx']) 
+    else:
+        samples = [dict(idx=idx, **sample) for idx, sample in enumerate(samples)]
+    prompt_type = 'cot'
+    # execute
+    if ('pred' not in samples[0]) or execute:
+        if "pal" in prompt_type:
+            executor = PythonExecutor(get_answer_expr="solution()")
+        else:
+            executor = PythonExecutor(get_answer_from_stdout=True)
+
+        for sample in tqdm(samples, desc="Execute"):
+            sample['pred'] = []
+            sample['report'] = []
+            for code in sample['code']:
+                pred, report = run_execute(executor, code, prompt_type, execute=True)
+                sample['pred'].append(pred)
+                sample['report'].append(report)
+
+    params = [(idx, pred, sample['gt']) for idx, sample in enumerate(samples) for pred in sample['pred']]
+
+    scores = []
+    timeout_cnt = 0 
+
+    with ProcessPool() as pool:
+        future = pool.map(math_equal_process, params, timeout=10)
+        iterator = future.result()
+        with tqdm(total=len(samples), desc="Evaluate") as progress_bar:
+            while True:
+                try:
+                    result = next(iterator)
+                    scores.append(result)
+                except StopIteration:
+                    break
+                except TimeoutError as error:
+                    print(error)
+                    scores.append(False)
+                    timeout_cnt += 1
+                except Exception as error:
+                    print(error.traceback)
+                    exit()
+                progress_bar.update(1) 
+
+    idx = 0
+    score_mat = []
+    for sample in samples:
+        sample['score'] = scores[idx: idx+len(sample['pred'])]
+        assert len(sample['score']) == len(sample['pred'])
+        score_mat.append(sample['score'])
+        idx += len(sample['pred'])
+
+    max_len = max([len(s) for s in score_mat])
+
+    for i, s in enumerate(score_mat):
+        if len(s) < max_len:
+            score_mat[i] = s + [s[-1]] * (max_len - len(s)) # pad
+
+    return [int(sam['score'][0]) for sam in samples]
+    
 def evaluate(data_name, prompt_type, samples: list=None, file_path: str=None, max_num_samples=None, execute=False):
     assert samples or file_path, "samples or file_path must be provided"
     if not samples:
